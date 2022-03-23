9394f9ee435e934b74ff536ef9af2dac23bc8730
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-23 17:55:33 -0300

    header media querie styled and add functions

--- scripts/header.js
+++ scripts/header.js
@@ -1,6 +1,26 @@
+let header = document.querySelector(".header");
 let searchForm = document.querySelector(".searchForm");
+let navbar = document.querySelector(".navbar");
 let searchBtn = document.querySelector("#searchBtn");
+let menuBtn = document.querySelector("#menuBtn");
+
+window.onscroll = () => {
+    navbar.classList.remove("active");
+    searchForm.classList.remove("active");
+
+    if (window.screenY > 0) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
+};
+
+menuBtn.onclick = () => {
+    navbar.classList.toggle("active");
+    searchForm.classList.remove("active");
+};
 
 searchBtn.onclick = () => {
     searchForm.classList.toggle("active");
+    navbar.classList.remove("active");
 };
