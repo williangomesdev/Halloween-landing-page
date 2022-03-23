f8f47f97bd0d728d4cca5e7aa1f747cf7f3cd391
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-23 00:19:16 -0300

    routes file

---
+++ scripts/routes.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const routes = express();
+
+
+
+export default routes;
--- scripts/server.js
+++ scripts/server.js
@@ -1,6 +1,8 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const routes = require("./routes");
 
+app.use(routes);
 
 app.listen(8080);
 console.log("server Rodando");
