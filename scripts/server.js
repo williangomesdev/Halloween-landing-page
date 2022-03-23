4392edd3ce960d3390dd7f4c48772fc298896480
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-23 01:11:52 -0300

    routes path files

--- scripts/server.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = express();
-const routes = require("./routes");
-
-app.use(routes);
-
-app.listen(8080);
-console.log("server Rodando");
---
+++ src/server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+const useRouter = require("./routes");
+
+app.set("view engine", "ejs");
+
+app.use(express.static("../public"));
+app.use(useRouter);
+
+app.listen(4000);
+console.log("server Rodando");
