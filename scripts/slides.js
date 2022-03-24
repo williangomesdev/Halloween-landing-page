8b8f1e9394c61abb8e751eda39cbe03dd32e1c56
Willian Amaro Gomes <willianamaroti@gmail.com>
2022-03-24 09:17:34 -0300

    home slider styled

---
+++ scripts/slides.js
@@ -0,0 +1,14 @@
+var swiper = new Swiper(".homeSlider", {
+    spaceBetween: 20,
+    effect: "fade",
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    centeredSlides: true,
+    autoplay: {
+        delay: 9500,
+        disableOnInteraction: false,
+    },
+});
